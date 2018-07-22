b8d821e323100fb4659d01c3c74689b2b19c1728
Harsh Vijay <36041550+Harshv99i@users.noreply.github.com>
2018-07-22 18:56:02 +0530

    Update connection.php
    changed connection method

--- connection.php
+++ connection.php
@@ -1,7 +1,17 @@
-<?php  
-       $servername = "localhost";  
-       $username = "root";  
-       $password = "";  
-       $conn = mysql_connect ($servername , $username , $password) or die("unable to connect to host");  
-       $sql = mysql_select_db ('test',$conn) or die("unable to connect to database"); 
-?> 
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "my_db";
+$dsn = "mysql:host=$host;dbname=$database";
+try {
+$conn = new PDO( $dsn, $username, $password );
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//$conn = null;
+}
+catch (PDOException $e) {
+$conn = null;
+exit("Connection failed: " . $e->getMessage());
+}
+?>
